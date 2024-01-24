c341c03b756cc8c1cdb7bc43ec17f1d62f68c2c8
pini-winer <95618690+pini-winer@users.noreply.github.com>
2024-01-24 08:29:04 +0200

    Update burger.js

--- js/burger.js
+++ js/burger.js
@@ -3,9 +3,6 @@ function burger_init(){
   let nav_burger = document.querySelector("#nav_burger");
 
   burger_btn.addEventListener("click", function(){
-    // אם נאב בורגר במצב דיספליי שונה מבלוק כלומר בלתי נראה
-    // יהפוך לבלוק
-    // אחרת הופך לנאן - בלתי נראה
     if(nav_burger.className == "nav_bar"){
       nav_burger.className = "nav_bar nav_bar_inside"
       // nav_burger.style.display = "block";
